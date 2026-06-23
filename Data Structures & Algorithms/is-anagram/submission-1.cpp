483feac62120dfc7d70e03fd5a04d0e5ddd67280
vinceeguico <131414686+vinceeguico@users.noreply.github.com>
2026-06-23 15:48:19 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        unordered_map<char, int> chars;
+        for (const char c : s) chars[c]++;
+        for (const char c : t) chars[c]--;
+        for (const auto it : chars) if (it.second != 0) return false;
+        return true;
+        
+    }
+};
