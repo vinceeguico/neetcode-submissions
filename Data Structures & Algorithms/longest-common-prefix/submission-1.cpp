76bdd084a5aff1c8ebf197be1f474eb0e0f8ce9f
vinceeguico <131414686+vinceeguico@users.noreply.github.com>
2026-07-03 16:08:47 -0700

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+        string pre = "";
+        
+        for (size_t i = 0; i < strs[0].size(); i++) {
+
+            for (size_t j = 1; j < strs.size(); j++) {
+
+                cout << "checking: " << strs[j][i] << ", against: " << strs[0][i] << endl;
+
+                if (strs[j][i] != strs[0][i]) {
+
+                    return pre;
+
+                }
+
+            }
+            pre += strs[0][i];
+
+        }
+
+        return pre;
+
+    }
+};
